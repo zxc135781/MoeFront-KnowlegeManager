cf3b155892789416631bcbadc1071a942b4ee373
Tyan Boot <tyanboot@outlook.com>
2015-07-18 23:01:16 +0800

    修正缩进
    Signed-off-by: Tyan Boot <tyanboot@outlook.com>

--- Template/editor.php
+++ Template/editor.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* 知识笔记管理 RIA MarkDown 编辑器页面
-*/
+ * 知识笔记管理 RIA MarkDown 编辑器页面
+ */
 include 'header.php';
 ?>
--- Template/header.php
+++ Template/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE HTML PUBLIC>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-	<link rel="stylesheet" type="text/css" href="Include/CSS/Style.css">
-	<title><?php echo $this->getOption('title',0); ?></title>
-	<script type="text/javascript" src="Include/JS/jquery.js"></script>
-	<script type="text/javascript" src="Include/JS/Script.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Include/CSS/Style.css">
+    <title><?php echo $this->getOption('title', 0); ?></title>
+    <script type="text/javascript" src="Include/JS/jquery.js"></script>
+    <script type="text/javascript" src="Include/JS/Script.js"></script>
 </head>
 <body>
